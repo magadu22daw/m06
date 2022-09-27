120e2d871aae8ca38ca0e7b7edd2a15c0287707a
magadu22daw <114517396+magadu22daw@users.noreply.github.com>
2022-09-27 22:28:16 +0200

    Add files via upload

--- exercici1-3.js
+++ exercici1-3.js
@@ -1,14 +1,13 @@
-
-
 window.onload = function () {
     let capital = ["Paris", "Londres", "Roma", "Berlin", "Copenhaguen", "Viena" ,"Lisboa", "Washington_DC" ,"Otawa", "L'Havana", "Santiago", "Lima","Montevideo", "Canberra", "Wellington", "Monròvia" ,"Abuja", "Dakar" ,"Tunis", "Tokyo" ,"Seül", "Beijing", "Taipei"];
     let pais = ["França", "UK", "Itàlia", "Alemanya", "Dinamarca", "Àustria", "Portugal", "USA", "Canada", "Cuba", "Xile", "Perú", "Uruguay", "Austràlia", "Nova_" ,"Zelanda", "Libèria", "Nigèria" ,"Senegal", "Tunísia", "Japó" ,"Corea_Sud" ,"Xina", "Taiwan"];
     let continent = ["Europa", "Europa", "Europa", "Europa", "Europa", "Europa", "Europa", "Amèrica", "Amèrica", "Amèrica", "Amèrica" ,"Amèrica", "Amèrica", "Oceania", "Oceania", "Àfrica", "Àfrica", "Àfrica", "Àfrica", "Àsia", "Àsia" ,"Àsia"];
-
+    
     document.getElementById('mostraTaula').onclick = function () {
+        document.getElementById('taula').innerHTML = `<tr><td>Capital</td><td>Pais</td><td>Continent</td></td>`
         let n1 = parseInt(document.getElementById('n1').value);
         for(let i=0;i<n1;i++){
-            let rN = Math.floor(Math.random() * 23);
+            let rN = Math.floor(Math.random() * capital.length);
             document.getElementById('taula').innerHTML += 
             `<tr><td> ${capital[rN]} </td><td> ${pais[rN]} </td><td> ${continent[rN]} </td></tr>`;
 
